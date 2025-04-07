715f3fb46c6e0b4320dacc23320524138fb1cf28
Pamilerin Marcus Oladunjoye <pamoladize10@gmail.com>
2025-04-07 10:38:58 +0100

    Fix: reduced minimum length of username in backend model

--- server/model/User.js
+++ server/model/User.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique:true,
-        minLength: 6
+        minLength: 3
     },
     password: {
         type: String,
